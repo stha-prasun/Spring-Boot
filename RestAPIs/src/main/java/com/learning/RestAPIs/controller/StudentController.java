94be6f9cce793976ccefc889db0af65687792a23
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-02-06 13:09:00 +0545

    Connection with Postgres

--- RestAPIs/src/main/java/com/learning/RestAPIs/controller/StudentController.java
+++ RestAPIs/src/main/java/com/learning/RestAPIs/controller/StudentController.java
@@ -1,15 +1,25 @@
 package com.learning.RestAPIs.controller;
 
 import com.learning.RestAPIs.dto.StudentDto;
+import com.learning.RestAPIs.entity.Student;
+import com.learning.RestAPIs.repository.StudentRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController //RestController helps to return JSON
 
 public class StudentController {
 
+    private final StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @GetMapping("/student")
-    public StudentDto getStudent(){
-        return new StudentDto(4L, "Prasun", "stha.prasun8@gmail.com");
+    public List<Student> getStudent(){
+        return studentRepository.findAll();
     }
 }
---
+++ RestAPIs/src/main/java/com/learning/RestAPIs/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.learning.RestAPIs.repository;
+
+import com.learning.RestAPIs.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
