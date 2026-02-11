d3db74a470050fd6860a66bb284fd30ac814c377
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-02-11 11:59:51 +0545

    Entity and Repo

---
+++ HospitalManagement/src/main/java/com/Prasun/HospitalManagement/Entity/Patient.java
@@ -0,0 +1,30 @@
+package com.Prasun.HospitalManagement.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    private String email;
+
+    private String gender;
+}
---
+++ HospitalManagement/src/test/java/com/Prasun/HospitalManagement/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prasun.HospitalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
