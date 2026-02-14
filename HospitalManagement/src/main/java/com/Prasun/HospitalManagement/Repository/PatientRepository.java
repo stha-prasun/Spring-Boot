0c603073adb7c5c980371bd92a56a90871ac442a
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-02-14 12:15:00 +0545

    Update

--- HospitalManagement/src/main/java/com/Prasun/HospitalManagement/Repository/PatientRepository.java
+++ HospitalManagement/src/main/java/com/Prasun/HospitalManagement/Repository/PatientRepository.java
@@ -1,7 +1,9 @@
 package com.Prasun.HospitalManagement.Repository;
 
 import com.Prasun.HospitalManagement.Entity.Patient;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -20,4 +22,11 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
     //Native Query
     @Query(value = "select * from patient", nativeQuery = true)
     List<Patient> findAllPatients();
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Patient p SET p.name = :name WHERE p.id = :id")
+    int updateNameWithId(@Param("name") String name,
+                         @Param("id") Long id);
+
 }
