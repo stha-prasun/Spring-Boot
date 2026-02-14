02acf2043d199b6cef077d0a3c06b75588e4245f
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-02-14 14:36:00 +0545

    Appointment

---
+++ Project/src/main/java/com/Prasun/Project/Entity/Appointment.java
@@ -0,0 +1,28 @@
+package com.Prasun.Project.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime appointmentTime;
+
+    @Column(length = 500)
+    private String reason;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id", nullable = false) // patient is required and not nullable
+    private Patient patient;
+}
