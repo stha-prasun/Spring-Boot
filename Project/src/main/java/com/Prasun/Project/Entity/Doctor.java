e65e0f45ac09679cc8276f24d8458ef2f18e1d28
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-02-15 12:20:06 +0545

    Changes

--- Project/src/main/java/com/Prasun/Project/Entity/Doctor.java
+++ Project/src/main/java/com/Prasun/Project/Entity/Doctor.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Data
@@ -27,5 +29,8 @@ public class Doctor {
 
     @OneToMany(mappedBy = "doctor")
     private List<Appointment> appointments;
+
+    @ManyToMany(mappedBy = "doctors")
+    private Set<Department> departments = new HashSet<>();
 
 }
