55793181e50f8520fb6591b2093a29c0d958cf5f
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-02-15 12:16:27 +0545

    Department

---
+++ Project/src/main/java/com/Prasun/Project/Entity/Department.java
@@ -0,0 +1,28 @@
+package com.Prasun.Project.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @OneToOne
+    private Doctor headDoctor;
+
+    @ManyToMany
+    private Set<Doctor> doctors = new HashSet<>();
+}
